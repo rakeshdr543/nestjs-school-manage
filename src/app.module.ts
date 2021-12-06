937bfc2555962b21d261a8b4588f27749a5233ea
rakeshdr543 <rakeshdr543@gmail.com>
2021-12-06 14:40:48 +0530

    Added student module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 import { Lesson } from './lesson/lesson.entity';
+import { Student } from './student/student.entity';
 import { StudentModule } from './student/student.module';
 
 @Module({
@@ -12,7 +13,7 @@ import { StudentModule } from './student/student.module';
       url: 'mongodb+srv://rakesh:12345@cluster0.oaxa7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
       synchronize: true,
       useUnifiedTopology: true,
-      entities: [Lesson],
+      entities: [Lesson, Student],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Student } from './student.entity';
+import { StudentService } from './student.service';
+import { StudentResolver } from './student.resolver';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Student])],
+  providers: [StudentService, StudentResolver],
+})
 export class StudentModule {}
